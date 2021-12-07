fc44d496559c8a5ae8c85e1f4e9a452488a5ecbe
Jeff Cela <cela.j@labrador-company.com>
2021-12-06 22:11:52 -0500

    fixed bug

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,53 +1,53 @@
-var urlsToCache = [
-    "/",
-    "/db.js",
-    "/index.js",
-    "/manifest.json",
-    "/styles.css",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
-  ];
-
-var CACHE_NAME = "tracker-cache-v1";
+var CACHE_NAME = "my-site-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+var urlsToCache = [
+  "/",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
 self.addEventListener("install", function(event) {
-    event.waitUntil(
-      caches.open(CACHE_NAME).then(function(cache) {
-        console.log("Opened cache");
-        return cache.addAll(urlsToCache);
-      })
-    );
-  });
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
 
-  self.addEventListener("fetch", function(event) {
-    if (event.request.url.includes("/api/")) {
-      event.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
-          return fetch(event.request)
-            .then(response => {
-              if (response.status === 200) {
-                cache.put(event.request.url, response.clone());
-              }
-              return response;
-            })
-            .catch(err => {
-              return cache.match(event.request);
-            });
-        }).catch(err => console.log(err))
-      );
-      return;
-    }
-
+self.addEventListener("fetch", function(event) {
+  if (event.request.url.includes("/api/")) {
     event.respondWith(
-      fetch(event.request).catch(function() {
-        return caches.match(event.request).then(function(response) {
-          if (response) {
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
             return response;
-          } else if (event.request.headers.get("accept").includes("text/html")) {
-            return caches.match("/");
-          }
-        });
-      })
+          })
+          .catch(err => {
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
     );
-  });
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
