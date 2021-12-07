6259babcfa61c482d88d691b9c3f7a3c6f45772c
Jeff Cela <cela.j@labrador-company.com>
2021-12-06 22:44:11 -0500

    attempt to fix error

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,41 +1,50 @@
-var CACHE_NAME = "my-site-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1";
-
-var urlsToCache = [
+const FILES_TO_CACHE = [
   "/",
-  "/db.js",
   "/index.js",
-  "/manifest.json",
+  "/db.js",
   "/styles.css",
+  "/manifest.json",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png"
 ];
 
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+//install
 self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+
+  self.skipWaiting();
 });
 
+// fetch
 self.addEventListener("fetch", function(event) {
+// cache successful requests to the API
   if (event.request.url.includes("/api/")) {
     event.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(event.request)
           .then(response => {
+            // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
               cache.put(event.request.url, response.clone());
             }
+
             return response;
           })
           .catch(err => {
+            // Network request failed, try to get it from the cache.
             return cache.match(event.request);
           });
       }).catch(err => console.log(err))
     );
+
     return;
   }
 
