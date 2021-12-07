a0220ae5d2bd26ef3e3278aa7ef9db0b26350cd6
Jeff Cela <cela.j@labrador-company.com>
2021-12-06 21:35:24 -0500

    created a service worker file, added in the cached items and the install function

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+var urlsToCache = [
+    "/",
+    "/db.js",
+    "/index.js",
+    "/manifest.json",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+  ];
+
+var CACHE_NAME = "tracker-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
