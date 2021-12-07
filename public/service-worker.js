4fe135375c1a78ac3f5f9889a8939466f1504cd8
Jeff Cela <cela.j@labrador-company.com>
2021-12-06 21:45:06 -0500

    added in the cache all requests

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,3 +10,37 @@ var urlsToCache = [
 
 var CACHE_NAME = "tracker-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+
+// install
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(function(cache) {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
+    );
+  });
+
+  //fetch
+  self.addEventListener("fetch", function(event) {
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+});
