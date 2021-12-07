3e4c253d0f446c8ec5104e1e02bacf8fd68a6840
Jeff Cela <cela.j@labrador-company.com>
2021-12-06 22:48:28 -0500

    update to db connetions

--- server.js
+++ server.js
@@ -15,14 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetTracker", {
+mongoose.connect( 
+  process.env.MONGODB_URI || "mongodb://localhost/budget", 
+  {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
   useCreateIndex: true
 });
 
-// routes
+
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
